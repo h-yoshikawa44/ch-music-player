71fc6d45fe426e92edce4a42aa158a5a9174bfac
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2024-04-13 17:19:21 +0900

    feat: 曲再生と停止の仕組みを作成(#5)

--- js/main.js
+++ js/main.js
@@ -1,2 +1,31 @@
+/* global document, HTMLAudioElement */
+
 import '../css/reset.css';
 import '../css/style.css';
+
+const hiddenClass = 'hidden';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const audio = document.getElementById('audio');
+  const playButton = document.getElementById('player-button');
+  const pauseButton = document.getElementById('pause-button');
+
+  const playMusic = () => {
+    if (!(audio instanceof HTMLAudioElement)) return;
+
+    audio.play();
+    playButton.classList.add(hiddenClass);
+    pauseButton.classList.remove(hiddenClass);
+  };
+
+  const pauseMusic = () => {
+    if (!(audio instanceof HTMLAudioElement)) return;
+
+    audio.pause();
+    pauseButton.classList.add(hiddenClass);
+    playButton.classList.remove(hiddenClass);
+  };
+
+  playButton.addEventListener('click', playMusic);
+  pauseButton.addEventListener('click', pauseMusic);
+});
